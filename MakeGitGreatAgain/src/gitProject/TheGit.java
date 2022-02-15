a9d99f46ad9b240380d02a0d247a0945ade6b752
DayWD <DawidTVT@o2.pl>
2022-02-15 15:37:57 +0100

    Add DayWDClass

--- MakeGitGreatAgain/src/gitProject/TheGit.java
+++ MakeGitGreatAgain/src/gitProject/TheGit.java
@@ -3,6 +3,7 @@ package gitProject;
 public class TheGit {
 
     public static void main(String[] args) {
-        System.out.println("LOL");
+        System.out.println("Start the Cyrk");
+        DayWDClass.saySomething();
     }
 }
